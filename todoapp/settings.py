635cbd35b7813d96e5735dc0fe584e59687e7625
marcoiligan <miligan17@gmail.com>
2023-03-25 20:20:13 +0800

    fixed db and environment variables

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,9 +79,13 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'mysql.connector.django',
+        'NAME':os.environ.get('db_name'),
+        'USER':os.environ.get('db_user'),
+        'PASSWORD':os.environ.get('db_password'),
+        'HOST':os.environ.get('db_host'),
+        'PORT':os.environ.get('db_port'),
     }
 }
 
